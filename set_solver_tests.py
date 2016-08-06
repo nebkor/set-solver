18f9e22c08cfa2503a7a27bb48600e0a9d5245af
jake-jake-jake <jcdavis@awedge.net>
2016-08-06 13:53:56 -0400

    Add test for schema creation

--- set_solver_tests.py
+++ set_solver_tests.py
@@ -1,5 +1,6 @@
 # usr/bin/env python
 
+import random
 import unittest
 
 from set_solver import SetSolver
@@ -20,6 +21,10 @@ class TestConstructSolver(unittest.TestCase):
 
     def test_with_attributes(self):
         ''' SetSolver should accept a dictionary of attributes.'''
+        attribs = {'colors': ['red', 'blue', 'yellow'],
+                   'shape':  ['circle', 'square', 'diamond'],
+                   'fill':   ['none', 'stripe', 'solid'],
+                   'number': [num for num in range(3)]}
         a_solver = SetSolver(attribs)
         self.assertIsNotNone(a_solver.attributes)
 
@@ -38,6 +43,41 @@ class TestConstructSolver(unittest.TestCase):
         self.assertRaises(TypeError, 
                           SetSolver, attribs_not_list,
                           msg='Attributes must be list.')
+
+class TestSolverActions(unittest.TestCase):
+
+    def test_schema_creation(self):
+        ''' Test scoring schema creation for varying hand sizes. '''
+
+        two_hand   = {'colors': ['red', 'blue'],
+                      'shape':  ['circle', 'square'],
+                      'fill':   ['none', 'stripe'],
+                      'number': ['one', 'two']}
+        three_hand = {'colors': ['red', 'blue', 'yellow'],
+                      'shape':  ['circle', 'square', 'diamond'],
+                      'fill':   ['none', 'stripe', 'solid'],
+                      'number': ['one', 'two', 'three']}
+        four_hand  = {'colors': ['red', 'blue', 'yellow', 'green'],
+                      'shape':  ['circle', 'square', 'diamond', 'oval'],
+                      'fill':   ['none', 'stripe', 'solid', 'polkadot'],
+                      'number': ['one', 'two', 'three', 'four']}
+        
+        # And some randomly generated n-dimensional Set game
+        # the 'numbers' key is necessary for the SetSolver class 
+        n = random.randrange(5, 11)
+        n_hand = {key: [value for value in range(n)]
+                  for key in range(n)}
+        n_hand['number'] = [value for value in range(n)]
+
+        two_set = SetSolver(two_hand)
+        three_set = SetSolver(three_hand)
+        four_set = SetSolver(four_hand)
+        n_set = SetSolver(n_hand)
+
+        self.assertIsNotNone(two_set.make_validation_schema())
+        self.assertIsNotNone(three_set.make_validation_schema())
+        self.assertIsNotNone(four_set.make_validation_schema())
+        self.assertIsNotNone(n_set.make_validation_schema())
 
 if __name__ == '__main__':
     unittest.main()
