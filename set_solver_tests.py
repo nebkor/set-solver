7f47cde9ea8697c074a3aa9c31dfc141bd4303cc
jake-jake-jake <jcdavis@awedge.net>
2016-08-06 14:13:47 -0400

    Add schema validation tests

--- set_solver_tests.py
+++ set_solver_tests.py
@@ -79,5 +79,81 @@ class TestSolverActions(unittest.TestCase):
         self.assertIsNotNone(four_set.make_validation_schema())
         self.assertIsNotNone(n_set.make_validation_schema())
 
+    def test_schema_validity(self):
+        ''' Validate schema scoring for three, four, and five card games.'''
+        three_hand = {'colors': ['red', 'blue', 'yellow'],
+                      'shape':  ['circle', 'square', 'diamond'],
+                      'fill':   ['none', 'stripe', 'solid'],
+                      'number': ['one', 'two', 'three']}
+        four_hand  = {'colors': ['red', 'blue', 'yellow', 'green'],
+                      'shape':  ['circle', 'square', 'diamond', 'oval'],
+                      'fill':   ['none', 'stripe', 'solid', 'polkadot'],
+                      'number': ['one', 'two', 'three', 'four']}
+        five_hand  = {'colors': ['red', 'blue', 'yellow', 'green', 'purple'],
+                      'shape':  ['circle', 'square', 'diamond', 'oval', 'zig'],
+                      'fill':   ['none', 'stripe', 'solid', 'polkadot', 'zag'],
+                      'number': ['one', 'two', 'three', 'four', 'five']}
+
+        valid_3    = {'colors': {'red': 1,
+                                 'blue': 3,
+                                 'yellow': 9},
+                      'shape':  {'circle': 1,
+                                 'square': 3,
+                                 'diamond': 9},
+                      'fill':   {'none': 1,
+                                 'stripe': 3, 
+                                 'solid': 9},
+                      'number': {'one': 1,
+                                 'two': 3,
+                                 'three': 9}
+                     }
+
+        valid_4    = {'colors': {'red': 1,
+                                 'blue': 4,
+                                 'yellow': 16,
+                                 'green': 64},
+                      'shape':  {'circle': 1,
+                                 'square': 4,
+                                 'diamond': 16,
+                                 'oval': 64},
+                      'fill':   {'none': 1,
+                                 'stripe': 4,
+                                 'solid': 16,
+                                 'polkadot': 64},
+                      'number': {'one': 1,
+                                 'two': 4,
+                                 'three': 16,
+                                 'four': 64}
+                     }
+
+        valid_5    = {'colors': {'red': 1,
+                                 'blue': 5,
+                                 'yellow': 25,
+                                 'green': 125,
+                                 'purple': 625}, 
+                      'shape':  {'circle': 1,
+                                 'square': 5, 
+                                 'diamond': 25,
+                                 'oval': 125,
+                                 'zig': 625},
+                      'fill':   {'none': 1,
+                                 'stripe': 5, 
+                                 'solid': 25,
+                                 'polkadot': 125,
+                                 'zag': 625},
+                      'number': {'one': 1,
+                                 'two': 5,
+                                 'three': 25,
+                                 'four': 125,
+                                 'five': 625}
+                     }
+
+        three_set = SetSolver(three_hand)
+        four_set = SetSolver(four_hand)
+        five_set = SetSolver(five_hand)
+        self.assertDictEqual(three_set.make_validation_schema(), valid_3)
+        self.assertDictEqual(four_set.make_validation_schema(), valid_4)
+        self.assertDictEqual(five_set.make_validation_schema(), valid_5)
+
 if __name__ == '__main__':
     unittest.main()
